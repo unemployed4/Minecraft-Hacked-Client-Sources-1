77ab70e620217456c3b11f0405a19945b2455087
unemployed4 <163641386+unemployed4@users.noreply.github.com>
2024-05-17 21:32:27 +0200

    Delete Envy 0.3 directory

--- Envy 0.3/README.md
+++
@@ -1,2 +0,0 @@
-# Envy
-This is an 1.8.8 Minecraft Hacked Client. Remove the AltLogin code from Envy.java before compiling client.
--- Envy 0.3/me/ihaq/iClient/event/events/EventSafe.java
+++
@@ -1,7 +0,0 @@
-package me.ihaq.iClient.event.events;
-
-import me.ihaq.iClient.event.Event;
-
-public class EventSafe extends Event{
-
-}
